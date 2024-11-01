ac3cd015aab7604a2cb35e0474dc57e15c4e11d7
Hosung <ssing365@naver.com>
2024-11-02 00:46:47 +0900

    [Crypto Tracker]Coin Data Types (+ deploy)

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -25,21 +25,91 @@ const Title = styled.h1`
 interface RouteParams {
   coinId: string;
 }
-
-interface RouteState{
-    name:string
+interface RouteState {
+  name: string
+}
+interface infoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  contract: string;
+  platform: string;
+  contracts: object;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    }
+  };
 }
 
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  const {state} = useLocation<RouteState>();
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState<infoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceInfo = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setLoading(false);
+      setInfo(infoData);
+      setPriceInfo(priceInfo);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading.."}</Title>
       </Header>
-      {loading ? <Loader>loading...</Loader> : null}
+      {loading ? <Loader>loading...</Loader> : priceInfo?.quotes.USD.ath_price } {/*TS가 priceinfo가 object에서 오는걸 아니까 자동으로 ?를 붙여준다. */}
     </Container>
   );
 }
