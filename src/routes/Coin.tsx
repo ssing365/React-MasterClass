39f34322832c06f3a757eed8245d9facb8529921
Lee <ssing365@naver.com>
2024-11-01 19:27:31 +0900

    [Crypto Tracker]Route States

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,47 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px; //모바일처럼 콘텐츠가 화면 가운데 위치
+  margin: 0 auto; //모바일처럼 콘텐츠가 화면 가운데 위치
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Loader = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
 
 interface RouteParams {
-    coinId:string;
+  coinId: string;
 }
 
-function Coin(){
-    const { coinId } = useParams<RouteParams>();
-    return <h1>Coin : {coinId}</h1>
+interface RouteState{
+    name:string
+}
+
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const {state} = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading.."}</Title>
+      </Header>
+      {loading ? <Loader>loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
